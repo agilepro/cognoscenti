70b17664fa6f7cb73005d9b6675d524e1fe146b5
Keith Swenson <kswenson@us.fujitsu.com>
2018-09-30 21:13:32 -0700

    only send ICS attachment when it is in the future

--- src/org/socialbiz/cog/EmailGenerator.java
+++ src/org/socialbiz/cog/EmailGenerator.java
@@ -310,8 +310,10 @@ public class EmailGenerator extends DOMFace {
                 }
             }
         }
+        
         //always attach the ICS file whether they ask for it or not
-        if (meeting!=null) {
+        //but only if the meeting is scheduled for some time in the future
+        if (meeting!=null && meeting.getStartTime()>System.currentTimeMillis()) {
             File projectFolder = ngp.containingFolder;
             File cogFolder = new File(projectFolder, ".cog");
             File icsFile = new File(cogFolder, "meet"+meeting.getId()+".ics");
